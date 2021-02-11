eb4f007359b77cece8ca02ffeab08ad1e1382a8b
Melanie Nolker <melanie.nolker@kiewit.com>
2021-02-10 20:51:50 -0600

    filter date added

--- UFO-level-1/Static/js/app.js
+++ UFO-level-1/Static/js/app.js
@@ -1,15 +1,44 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+// Select the button
+var button = d3.select("#filter-btn");
 
-// Use d3 to update each cell's text with
-// ufoReport values (date/time, city, state, country, shape, comment)
-data.forEach((ufoReport) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+// Create event handlers 
+button.on("click", runEnter);
+// input.on("submit",runEnter);
+
+// Complete the event handler function to filter the table
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+
+
+    // Get a reference to the table body
+    var tbody = d3.select("tbody");
+
+    // Use d3 to update each cell's text with
+    // ufoReport values (date/time, city, state, country, shape, comment)
+
+
+    filteredData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-  });
+
+
+};

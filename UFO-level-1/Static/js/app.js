7f8e2cbff0b4a3c74a452927a0c15bbac0168e90
Melanie Nolker <melanie.nolker@kiewit.com>
2021-02-10 19:28:27 -0600

    table loaded but not filtered

--- UFO-level-1/Static/js/app.js
+++ UFO-level-1/Static/js/app.js
@@ -1,28 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// DELETE WHEN FINISHED: https://stackoverflow.com/questions/45439840/append-to-html-table-using-javascript-with-data-elements
-
-
-for(var i=0; i< tableData.length; i++){
-	var tableString = "<tr>";
-  	    tableString += "<td>" + arrayOne[i] + "</td>";
-    tableString += "<td>" + ((typeof arrayTwo[i] === 'undefined') ? '' : arrayTwo[i]) + "</td>";
-    tableString += "<td>" + ((typeof arrayThree[i] === 'undefined') ? '' : arrayThree[i])  + "</td>";
-    tableString += "<td>" + ((typeof arrayFour[i] === 'undefined') ? '' : arrayFour[i]) + "</td>";
-    tableString += "<tr>";
-  
-    $('table tbody').append(tableString);
-}
-
-    // $("#myModal").find(".table").append("<tr><td>"+valOne+"</td><td>"+valTwo+"</td><td>"+valThree+"</td><td>"+valFour+"</td></tr>");
-
-    for (var i = 0; i <arrayOne.length; i++ ){
-        var row = $('<tr/>').append($('<td/>', {text: arrayOne[i] || ''}))
-                .append($('<td/>', {text: arrayTwo[i] || ''}))
-                .append($('<td/>', {text: arrayThree[i] || ''}))
-                .append($('<td/>', {text: arrayFour[i] || ''}));
-        $('table tbody').append(row);
-    }
+// Use d3 to update each cell's text with
+// ufoReport values (date/time, city, state, country, shape, comment)
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
